5a1c5e56f34330eca57c3fa106a5535d8cf0f569
aSadGuy <asdshuja@hotmail.co.uk>
2021-12-31 13:46:18 +0000

    update

--- 3. ObjectsBasics/7.SymbolType.js
+++ 3. ObjectsBasics/7.SymbolType.js
@@ -1,2 +1,10 @@
 // Symbol Type
 console.log("--- SYMBOL TYPE ---");
+// Object property keys may be either of string type, or of symbol type, no other types
+// Symbol
+console.log("");
+console.log("--- SYMBOL ---");
+// A "symbol" represents a unique identifier
+// A value of this type can be created using "Symbol()"
+let id = Symbol("id");
+
