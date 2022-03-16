cb1ae0b995f390ed032ead434695da0b263ea6e7
aSadGuy <asdshuja@hotmail.co.uk>
2022-03-16 15:01:48 +0000

    finished 7.3

--- 7.Prototypes,inheritance/3.NativePrototypes.js
+++ 7.Prototypes,inheritance/3.NativePrototypes.js
@@ -64,3 +64,91 @@ console.log(arr); // 1,2,3 <-- the result of Array.prototype.toString
 // As seen before, "Object.prototype" has "toString" as well, but "Array.prototype"
 // is closer in the chain, so the array variant is used.
 
+// Other built-in objects also work the same way. Even functions – they are objects of
+// a built-in "Function" constructor, and their methods ("call"/"apply" and others)
+// are taken from "Function.prototype". Functions have their own "toString" too.
+function f() {}
+
+console.log(f.__proto__ == Function.prototype); // true
+console.log(f.__proto__.__proto__ == Object.prototype); // true, inherit from objects
+
+// Primitives
+console.log();
+console.log("--- PRIMITIVES ---");
+
+// The most intricate thing happens with strings, numbers and booleans.
+
+// They are not objects. But when trying to access their properties, temporary wrapper objects
+// are created using built-in constructors "String", "Number" and "Boolean".
+// They provide the methods and disappear.
+
+// These objects are created invisibly and most engines optimize them out, but the specification
+// describes it exactly this way. Methods of these objects also reside in prototypes,
+// available as "String.prototype", "Number.prototype" and "Boolean.prototype".
+
+// Changing Native Prototypes
+console.log();
+console.log("--- CHANGING NATIVE PROTOTYPES ---");
+
+// Native prototypes can be modified. For instance, a method is added to "String.prototype",
+// it becomes available to all strings:
+String.prototype.show = function () {
+  console.log(this);
+};
+
+"BOOM!".show(); // BOOM!
+
+// Adding new built-in methods to native prototypes is generally a bad idea.
+
+//~ In modern programming, there is only one case where modifying native prototypes is approved. That is polyfilling.
+
+// Polyfilling is a term for making a substitute for a method that exists in the JS specification,
+// but is not yet supported by a particular JS engine.
+
+// It may be implemented manually into the built-in prototype.
+// For instance:
+if (!String.prototype.repeat) {
+  // if there is no such method
+  // add it to the prototype
+
+  String.prototype.repeat = function (n) {
+    // repeat the string n times
+
+    // actually, the code should be a little bit more complex than that
+    // (the full algorithm is in the specification)
+    // but even an imperfect polyfill is often considered good enough
+    return new Array(n + 1).join(this);
+  };
+}
+console.log("La".repeat(3)); // LaLaLa
+
+// Borrowing From Prototypes
+console.log();
+console.log("--- BORROWING FROM PROTOTYPES ---");
+
+// In the chapter "5. AdvancedWorkingWithFunctions/9.DecoratorsAndForwarding.js", method borrowing was talked about.
+// That is when a method is taken from one object and copied into another.
+// Some methods of native prototypes are often borrowed.
+
+// For instance, if an array-like object is made, some "Array" methods may want to be copied to it:
+obj = {
+  0: "Hello",
+  1: "world!",
+  length: 2,
+};
+
+obj.join = Array.prototype.join;
+
+console.log(obj.join(" ")); // Hello world
+
+// It works because the internal algorithm of the built-in "join" method
+// only cares about the correct indexes and the "length" property. It does not check if
+// the object is indeed an array. Many built-in methods are like that.
+
+// Another possibility is to inherit by setting "obj.__proto__" to "Array.prototype",
+// so all "Array" methods are automatically available in "obj".
+
+// But that is impossible if "obj" already inherits from another object.
+//~ Only one object can be inherited at a time.
+
+// Borrowing methods is flexible, it allows to mix functionalities from different objects if needed.
